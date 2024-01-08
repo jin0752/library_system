fca24c11fe40c7811b9c49c0b943fc6123f9b7a5
albert <milky.bot1@gmail.com>
2024-01-08 22:49:15 +0800

    sa dashb

--- dashb.php
+++ dashb.php
@@ -3,7 +3,7 @@ session_start();
 
 // Check if the user is logged in
 if (!isset($_SESSION['username'])) {
-    header("Location: login.html");
+    header("Location: index.php");
     exit();
 }
 ?>

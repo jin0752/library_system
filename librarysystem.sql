c0792af0f7b1a24c3bf9c8f9eaf761ae9e8cd6e5
albert <milky.bot1@gmail.com>
2024-01-15 01:33:24 +0800

    new database

--- librarysystem.sql
+++ librarysystem.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 13, 2024 at 12:49 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Generation Time: Jan 14, 2024 at 06:32 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -42,7 +42,6 @@ CREATE TABLE `books` (
 --
 
 INSERT INTO `books` (`id`, `cabnum`, `title`, `author`, `year`, `bookver`, `booknum`) VALUES
-(1, 123131, 'fasfafsa', 'fafafa', 1231231, 'fahjkda', 1231321),
 (2, 0, 'Introduction to Linear Algebra with Application', 'Jim DeFranza, Daniel Gagliardi', 2019, 'QA 184.2 D43 2009 copy 1', 3565);
 
 -- --------------------------------------------------------
@@ -102,27 +101,29 @@ INSERT INTO `table_the_iot_projects` (`name`, `id`, `gender`, `email`, `mobile`)
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
   `fname` varchar(250) NOT NULL,
   `mname` varchar(250) NOT NULL,
   `lname` varchar(250) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `cpassword` varchar(250) NOT NULL
+  `cpassword` varchar(250) NOT NULL,
+  `verification_code` varchar(255) NOT NULL,
+  `is_verified` int(50) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `fname`, `mname`, `lname`, `email`, `password`, `cpassword`) VALUES
-(9, 'albert', '', '', '', 'albert', '$2y$10$2IUmIsh1mWJoZk.MbUM.du0e1IXR/OJi9SSIYE2ES1.wQQw1sfng2', ''),
-(10, 'rivera', '', '', '', 'rivera', '$2y$10$L8ikLsbPKKbkf94Rh.DO1u2g3.j7s6wDRe5W52ZHqKQwgFdM7rpgu', ''),
-(11, 'jin', '', '', '', 'jin123', '$2y$10$l0LSFC0697KiRhqnpWcYheTkRBhf1waeWGsFXzz1VWnmi/BPX7Z92', ''),
-(13, 'jin123', 'albert son', 'purificacion', 'albert son', 'albert@sample.com', '$2y$10$Np1QatwfRg.W9G3IKWepaubbiz6na9E92NDRNTdMrb0.d8UyJnVHa', ''),
-(14, 'admin', 'albert ', 'pds', 'albert ', 'dsadsa', '$2y$10$8Nm4hGMnNvKq8S0qTGv1ke60zPWbAM./LClQpV0cSgcCX1QbNQktS', '$2y$10$hKzkJqM2xoDHV51TgEEvj.1WV/SAC5vIc00g5PPgpB6Wf.cbdgj/q'),
-(21, 'test', 'admin', 'test', 'admin', 'test@sample.com', '$2y$10$8j3fXnL0fzD2BWSddda77.P/7roc.YvqdHuSe5/TYdhdaQCs5JcF.', '$2y$10$wu./kLHRtF3rE9IgOCg72ecxUTYrsj1gDpwJxeffTm4O08s///TIa'),
-(22, 'bon', 'bonna', 'giatay', 'bonna', 'bon@sample.com', '$2y$10$1vEqBNU9As48mMcx5e7HB.pYrsuGtFL.ki4BErZfBCV3cYWebnXWS', '$2y$10$7L5I//PESFlyoLbw.lTZ6emFNfEwbOgW9TAXn287D1ko/35cUwvuO');
+INSERT INTO `users` (`id`, `fname`, `mname`, `lname`, `email`, `password`, `cpassword`, `verification_code`, `is_verified`) VALUES
+(9, '', '', '', 'albert', '$2y$10$2IUmIsh1mWJoZk.MbUM.du0e1IXR/OJi9SSIYE2ES1.wQQw1sfng2', '', '', 0),
+(10, '', '', '', 'rivera', '$2y$10$L8ikLsbPKKbkf94Rh.DO1u2g3.j7s6wDRe5W52ZHqKQwgFdM7rpgu', '', '', 0),
+(11, '', '', '', 'jin123', '$2y$10$l0LSFC0697KiRhqnpWcYheTkRBhf1waeWGsFXzz1VWnmi/BPX7Z92', '', '', 0),
+(14, 'albert ', 'pds', 'albert ', 'dsadsa', '$2y$10$8Nm4hGMnNvKq8S0qTGv1ke60zPWbAM./LClQpV0cSgcCX1QbNQktS', '$2y$10$hKzkJqM2xoDHV51TgEEvj.1WV/SAC5vIc00g5PPgpB6Wf.cbdgj/q', '', 0),
+(21, 'admin', 'test', 'admin', 'test@sample.com', '$2y$10$8j3fXnL0fzD2BWSddda77.P/7roc.YvqdHuSe5/TYdhdaQCs5JcF.', '$2y$10$wu./kLHRtF3rE9IgOCg72ecxUTYrsj1gDpwJxeffTm4O08s///TIa', '', 0),
+(22, 'bonna', 'giatay', 'bonna', 'bon@sample.com', '$2y$10$1vEqBNU9As48mMcx5e7HB.pYrsuGtFL.ki4BErZfBCV3cYWebnXWS', '$2y$10$7L5I//PESFlyoLbw.lTZ6emFNfEwbOgW9TAXn287D1ko/35cUwvuO', '', 0),
+(77, 'albert', 'son', 'albert', 'jin.clips@gmail.com', '$2y$10$PeJ909tkKjCG22j2Zm6S6.lYRgDYdi1nfVpUAdc27ikfSIWc4E6l.', '$2y$10$5K4yyGcx3elYhAoQ20ZJF.sPLxhkvgj4sPvG1cA4UCXc226rGj3TO', 'a66934607d5cb110c44a91c85a42d25c', 0),
+(78, 'albert', 'son', 'albert', 'jin.clips@gmail.com', '$2y$10$PeJ909tkKjCG22j2Zm6S6.lYRgDYdi1nfVpUAdc27ikfSIWc4E6l.', '$2y$10$5K4yyGcx3elYhAoQ20ZJF.sPLxhkvgj4sPvG1cA4UCXc226rGj3TO', 'a66934607d5cb110c44a91c85a42d25c', 0);
 
 --
 -- Indexes for dumped tables
@@ -144,8 +145,7 @@ ALTER TABLE `table_the_iot_projects`
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username` (`username`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -161,7 +161,7 @@ ALTER TABLE `books`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=79;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
